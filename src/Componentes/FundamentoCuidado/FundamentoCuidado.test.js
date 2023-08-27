11d8191e26c7505e10e95e27a7cd2601057a5d69
Jose <naranjoalgarinj@gmail.com>
2023-08-27 17:57:31 -0500

    fundamentocuidado.test

--- src/Componentes/FundamentoCuidado/FundamentoCuidado.test.js
+++ src/Componentes/FundamentoCuidado/FundamentoCuidado.test.js
@@ -1,1 +1,17 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import FundamentoCuidado from './FundamentoCuidado';
 
+describe('FundamentoCuidado', () => {
+  it('renders correctly', () => {
+    const { getByText } = render(<FundamentoCuidado />);
+    
+    expect(getByText('Fundamentos del Cuidado de Plantas')).toBeInTheDocument();
+    expect(getByText('Luz:')).toBeInTheDocument();
+    expect(getByText('Agua:')).toBeInTheDocument();
+    expect(getByText('Sustrato:')).toBeInTheDocument();
+    expect(getByText('Clima y Humedad:')).toBeInTheDocument();
+    expect(getByText('Poda y Mantenimiento:')).toBeInTheDocument();
+    expect(getByText('Conclusión:')).toBeInTheDocument();
+  });
+});
