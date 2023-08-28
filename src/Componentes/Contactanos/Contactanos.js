6047852a3fb7863a409797d0f50af81d2d01ad17
Jose <naranjoalgarinj@gmail.com>
2023-08-27 20:03:39 -0500

    Contactanos js (Carpeta correcta)

---
+++ src/Componentes/Contactanos/Contactanos.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import './Contactanos.css'; // Asegúrate de cambiar el nombre al archivo de estilos correcto
+
+function ContactForm() {
+  const [submitted, setSubmitted] = useState(false);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Aquí puedes realizar cualquier acción necesaria antes de mostrar la alerta
+    setSubmitted(true);
+    alert("¡Gracias por tu mensaje! Nos pondremos en contacto contigo pronto.");
+  };
+
+  return (
+    <section id="container-form">
+      <div className="info-form">
+        <h2>Contactanos</h2>
+        <p>"¡Estamos aquí para escucharte! Contáctanos y permítenos ser parte de tu experiencia. Tu opinión y preguntas son importantes para nosotros."</p>
+        <a href="#container-form"> <i className="bi bi-telephone-fill"></i> 315-311-1350</a>
+        <a href="#container-form"> <i className="bi bi-envelope-at"></i> naranjoalgarinj@gmail.com</a>
+        <a href="#container-form"> <i className="bi bi-geo-alt-fill"></i> Sahagun, Cordoba</a>
+      </div>
+      <form action="#" autoComplete="off" onSubmit={handleSubmit}>
+        <input type="text" name="nombre" placeholder="Ingresa tu nombre" className="campo" />
+        <input type="email" name="email" placeholder="Ingresa tu correo" className="campo" />
+        <textarea name="mensaje" placeholder="Ingresa tu mensaje"></textarea>
+        <input type="submit" name="enviar" value="Enviar mensaje" className="btn-enviar" />
+      </form>
+    </section>
+  );
+}
+
+export default Contactanos;
