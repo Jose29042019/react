74cc5fb17c05791aacb9ee147192677d34aa3b8c
Jose <naranjoalgarinj@gmail.com>
2023-08-27 21:23:34 -0500

    Footer.js

---
+++ src/Componentes/Footer/Footer.js
@@ -0,0 +1,13 @@
+// Footer.js
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p className="texto-footer">© 2023 Jose Julian Naranjo</p>
+    </footer>
+  );
+}
+
+export default Footer;
